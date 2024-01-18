5b47fff96d67531cd7e6066297c2df319791781b
Tomi Viisanen <tomi_99@hotmail.fi>
2024-01-18 09:29:56 +0200

    yes

--- WorkflowTest/Program.cs
+++ WorkflowTest/Program.cs
@@ -11,7 +11,7 @@ namespace WorkflowTest
             Console.WriteLine("hello");
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddEndpointsApiExplorer(); 
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
